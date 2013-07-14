867247d37c12cc3207083ff8ec7a2f30f7e2e47e
Amit Feller <amit.feller@gmail.com>
2013-07-14 13:50:53 +0000

    adding support for --url option

--- grader.js
+++ grader.js
@@ -1,4 +1,4 @@
-!/usr/bin/env node
+#!/usr/bin/env node
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -45,7 +47,7 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-  $ = cheerioHtmlFile(htmlfile);
+  $ = htmlfile;
   var checks = loadChecks(checksfile).sort();
   var out = {};
   for(var ii in checks) {
@@ -61,14 +63,36 @@ var clone = function(fn) {
   return fn.bind({});
 };
 
+var checkAndPrint = function(file,checks)
+{
+  var checkJson = checkHtmlFile(file, checks);
+  var outJson = JSON.stringify(checkJson, null, 4);
+  console.log(outJson);
+}
+
 if(require.main == module) {
   program
   .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
   .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+  .option('-u, --url <html_url>', 'Path to url',null, null)
   .parse(process.argv);
-  var checkJson = checkHtmlFile(program.file, program.checks);
-  var outJson = JSON.stringify(checkJson, null, 4);
-  console.log(outJson);
+  if(program.url)
+  {
+    var response  = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            var file = cheerio.load(result);
+	    checkAndPrint(file,program.checks);
+        }
+    };
+    rest.get(program.url).on('complete', response);
+  } else {
+  	var htmlFile = cheerioHtmlFile(program.file);
+	checkAndPrint(htmlFile,program.checks);
+  }
+ 
 } else {
   exports.checkHtmlFile = checkHtmlFile;
 }
+
